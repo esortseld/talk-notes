c803bf3df23af553a879658e57849e1386c08c5f
esortseld <suzukimegumi0314@gmail.com>
2026-06-12 19:54:00 +0900

    Simplify live history data and add WAON setlist

--- data/lives.js
+++ data/lives.js
@@ -5,14 +5,31 @@ const lives = [
     venue: "Piacere",
     band: "カンガルーポケット",
     event: "Piacere Fes.",
-    memo: "カンガルーポケットのライブ実績サンプル。",
     setlist: [
-      "stop-stop-rocknroll",
-      "sono-speed-de",
-      "harenchi",
-      "nando-demo",
-      "10years",
-      "yumemiru-shoujo"
+      { no: "1", title: "Stop Stop Rock'n'Roll", artist: "山下久美子" },
+      { no: "2", title: "そのスピードで", artist: "the brilliant green" },
+      { no: "3", title: "ハレンチ", artist: "ちゃんみな" },
+      { no: "4", title: "何度でも", artist: "DREAMS COME TRUE" },
+      { no: "5", title: "10 years", artist: "渡辺美里" },
+      { no: "6", title: "夢見る少女じゃいられない", artist: "相川七瀬" }
+    ]
+  },
+  {
+    id: "2026-05-31-waon",
+    date: "2026-05-31",
+    venue: "WAON",
+    band: "林檎庵",
+    event: "cord of friend",
+    setlist: [
+      { no: "1", title: "幸福論", artist: "椎名林檎" },
+      { no: "2", title: "歌舞伎町の女王", artist: "椎名林檎" },
+      { no: "3", title: "ここでキスして。", artist: "椎名林檎" },
+      { no: "4", title: "正しい街", artist: "椎名林檎" },
+      { no: "5", title: "罪と罰", artist: "椎名林檎" },
+      { no: "6", title: "スーパースター", artist: "東京事変" },
+      { no: "7", title: "アイデンティティ", artist: "椎名林檎" },
+      { no: "8", title: "群青日和", artist: "東京事変" },
+      { no: "EN", title: "丸ノ内サディスティック", artist: "椎名林檎" }
     ]
   }
 ];
