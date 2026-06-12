4c76cb78bee36368a6880bc0ba86b1be7a7555a4
esortseld <suzukimegumi0314@gmail.com>
2026-06-12 20:14:20 +0900

    Link live setlists to song cards

--- data/lives.js
+++ data/lives.js
@@ -6,13 +6,13 @@ const lives = [
     band: "sweet pastime",
     event: "Brushup Live",
     setlist: [
-      { no: "1", title: "時間の国のアリス", artist: "松田聖子" },
-      { no: "2", title: "白い炎", artist: "斉藤由貴" },
-      { no: "3", title: "情熱", artist: "斉藤由貴" },
-      { no: "4", title: "C-Girl", artist: "浅香唯" },
-      { no: "5", title: "悲しみモニュメント", artist: "南野陽子" },
-      { no: "6", title: "スマイル・フォー・ミー", artist: "河合奈保子" },
-      { no: "7", title: "うしろゆびさされ組", artist: "うしろゆびさされ組" }
+      { no: "1", title: "時間の国のアリス", artist: "松田聖子", songId: "jikan-no-kuni-no-alice" },
+      { no: "2", title: "白い炎", artist: "斉藤由貴", songId: "shiroi-honoo" },
+      { no: "3", title: "情熱", artist: "斉藤由貴", songId: "jounetsu" },
+      { no: "4", title: "C-Girl", artist: "浅香唯", songId: "c-girl" },
+      { no: "5", title: "悲しみモニュメント", artist: "南野陽子", songId: "kanashimi-monument" },
+      { no: "6", title: "スマイル・フォー・ミー", artist: "河合奈保子", songId: "smile-for-me" },
+      { no: "7", title: "うしろゆびさされ組", artist: "うしろゆびさされ組", songId: "ushiroyubi-sasaregumi" }
     ]
   },
   {
@@ -22,12 +22,12 @@ const lives = [
     band: "カンガルーポケット",
     event: "Piacere Fes.",
     setlist: [
-      { no: "1", title: "Stop Stop Rock'n'Roll", artist: "山下久美子" },
-      { no: "2", title: "そのスピードで", artist: "the brilliant green" },
-      { no: "3", title: "ハレンチ", artist: "ちゃんみな" },
-      { no: "4", title: "何度でも", artist: "DREAMS COME TRUE" },
-      { no: "5", title: "10 years", artist: "渡辺美里" },
-      { no: "6", title: "夢見る少女じゃいられない", artist: "相川七瀬" }
+      { no: "1", title: "Stop Stop Rock'n'Roll", artist: "山下久美子", songId: "stop-stop-rocknroll" },
+      { no: "2", title: "そのスピードで", artist: "the brilliant green", songId: "sono-speed-de" },
+      { no: "3", title: "ハレンチ", artist: "ちゃんみな", songId: "harenchi" },
+      { no: "4", title: "何度でも", artist: "DREAMS COME TRUE", songId: "nando-demo" },
+      { no: "5", title: "10 years", artist: "渡辺美里", songId: "10years" },
+      { no: "6", title: "夢見る少女じゃいられない", artist: "相川七瀬", songId: "yumemiru-shoujo" }
     ]
   },
   {
@@ -37,15 +37,15 @@ const lives = [
     band: "林檎庵",
     event: "cord of friend",
     setlist: [
-      { no: "1", title: "幸福論", artist: "椎名林檎" },
-      { no: "2", title: "歌舞伎町の女王", artist: "椎名林檎" },
-      { no: "3", title: "ここでキスして。", artist: "椎名林檎" },
-      { no: "4", title: "正しい街", artist: "椎名林檎" },
-      { no: "5", title: "罪と罰", artist: "椎名林檎" },
-      { no: "6", title: "スーパースター", artist: "東京事変" },
-      { no: "7", title: "アイデンティティ", artist: "椎名林檎" },
-      { no: "8", title: "群青日和", artist: "東京事変" },
-      { no: "EN", title: "丸ノ内サディスティック", artist: "椎名林檎" }
+      { no: "1", title: "幸福論", artist: "椎名林檎", songId: "koufukuron" },
+      { no: "2", title: "歌舞伎町の女王", artist: "椎名林檎", songId: "kabukicho-no-joou" },
+      { no: "3", title: "ここでキスして。", artist: "椎名林檎", songId: "koko-de-kiss-shite" },
+      { no: "4", title: "正しい街", artist: "椎名林檎", songId: "tadashii-machi" },
+      { no: "5", title: "罪と罰", artist: "椎名林檎", songId: "tsumi-to-batsu" },
+      { no: "6", title: "スーパースター", artist: "東京事変", songId: "superstar" },
+      { no: "7", title: "アイデンティティ", artist: "椎名林檎", songId: "identity" },
+      { no: "8", title: "群青日和", artist: "東京事変", songId: "gunjou-biyori" },
+      { no: "EN", title: "丸ノ内サディスティック", artist: "椎名林檎", songId: "marunouchi-sadistic" }
     ]
   }
 ];
