bf6785c67393e2aad601997c99c7e62cce59e567
esortseld <suzukimegumi0314@gmail.com>
2026-06-12 19:48:10 +0900

    Add Kangaroo Pocket live history data

---
+++ data/lives.js
@@ -0,0 +1,18 @@
+const lives = [
+  {
+    id: "2026-04-18-piacere",
+    date: "2026-04-18",
+    venue: "Piacere",
+    band: "カンガルーポケット",
+    event: "Piacere Fes.",
+    memo: "カンガルーポケットのライブ実績サンプル。",
+    setlist: [
+      "stop-stop-rocknroll",
+      "sono-speed-de",
+      "harenchi",
+      "nando-demo",
+      "10years",
+      "yumemiru-shoujo"
+    ]
+  }
+];
